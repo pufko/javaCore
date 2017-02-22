3d8be2caff1e657c83f63472cb4d4d96394654a7
pufko <pufan4eg@gmail.com>
2017-02-22 02:06:20 +0200

    first commit

---
+++ FirstProgram.java
@@ -0,0 +1,9 @@
+/**
+ * Created by mr p on 22.02.2017.
+ */
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("hii");
+        System.out.println("ololo");    }
+}
---
+++ Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by mr p on 22.02.2017.
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}

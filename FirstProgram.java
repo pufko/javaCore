68fea113004bd5a7c64d184f153fc6e7bbba6ca8
pufko <pufan4eg@gmail.com>
2017-02-22 02:15:51 +0200

    second commit

--- FirstProgram.java
+++ FirstProgram.java
@@ -5,5 +5,5 @@ public class FirstProgram {
     public static void main(String[] args) {
         System.out.println("1");
         System.out.println("hii");
-        System.out.println("ololo");    }
+        System.out.println("ololo2");    }
 }

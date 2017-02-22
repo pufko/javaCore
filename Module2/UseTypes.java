4848febf101a647071f82f999bcc134441507013
pufko <pufan4eg@gmail.com>
2017-02-22 04:57:28 +0200

    Boolean

---
+++ Module2/UseTypes.java
@@ -0,0 +1,12 @@
+package Module2;
+
+/**
+ * Created by mr p on 22.02.2017.
+ */
+public class UseTypes {
+    public static void main(String[] args) {
+        boolean typeB = true;
+        typeB = false;
+        System.out.println(typeB);
+    }
+}

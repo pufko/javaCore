ceec852da060937f79e5e4d3aee92c91f3e73c22
pufko <pufan4eg@gmail.com>
2017-02-22 05:37:34 +0200

    UseOperator

---
+++ Module2/UseOperator.java
@@ -0,0 +1,15 @@
+package Module2;
+
+/**
+ * Created by mr p on 22.02.2017.
+ */
+public class UseOperator {
+    public static void main(String[] args) {
+
+        int integer1=50;
+        int integer2=10;
+
+        int integer3= integer1+integer2;
+        System.out.println(integer3);
+    }
+}
--- Module2/UseTypes.java
+++ Module2/UseTypes.java
@@ -7,6 +7,18 @@ public class UseTypes {
     public static void main(String[] args) {
         boolean typeB = true;
         typeB = false;
-        System.out.println(typeB);
+            System.out.println(typeB);
+        byte byteType = 127;
+        double doubleValue = 14.88;
+            System.out.println(doubleValue);
+        char charType = 't';
+        int testChar = charType;
+        System.out.println(charType);
+        System.out.println(testChar);
+
+        char charType2 = 42001;
+        System.out.println(charType2);
+
+
     }
 }
